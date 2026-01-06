aa82f9390fe6b9a62a68c988c8b8d4d8e6232352
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-06 15:30:08 -0800

    123

---
+++ vlm_finetune/copy_dataset.py
@@ -0,0 +1,59 @@
+import modal
+import subprocess
+import re
+import shutil
+
+
+from .common import axolotl_image, data_volume, CONFIG_FILE_PATH
+from pathlib import Path
+import json
+
+DATA_VOLUME_DIR = Path("/data")
+
+app = modal.App("axolotl-vlm-prep-dataset")
+
+@app.function(
+    image=axolotl_image,
+    gpu="H100",
+    volumes={DATA_VOLUME_DIR.as_posix(): data_volume},
+)
+def prep_dataset():
+
+    src_paths = [
+        "vlm_finetune/data/train_merchant_vs_global.jsonl",
+        "vlm_finetune/data/val_merchant_vs_global.jsonl",
+        "vlm_finetune/data/test_merchant_vs_global.jsonl",
+    ]
+
+    dst_paths = [
+        DATA_VOLUME_DIR / "mvg_train.jsonl",
+        DATA_VOLUME_DIR / "mvg_eval.jsonl",
+        DATA_VOLUME_DIR / "mvg_test.jsonl",
+    ]
+
+    # def convert_one(src: Path, dst: Path) -> None:
+    #     if not src.exists():
+    #         return
+        
+
+    #     with src.open("r", encoding="utf-8") as fin, dst.open("w", encoding="utf-8") as fout:
+    #         for line in fin:
+    #             if not line.strip():
+    #                 continue
+    #             src_obj = json.loads(line)
+    #             dst_obj = to_messages(src_obj)
+    #             fout.write(json.dumps(dst_obj, ensure_ascii=False) + "\n")
+
+    for p in range(len(src_paths)):
+        shutil.copy(src_paths[p], dst_paths[p])
+
+
+    subprocess.run(
+        [
+            "axolotl",
+            "preprocess",
+            CONFIG_FILE_PATH.as_posix(),
+        ],
+        check=True,
+    )
+    
