737c7f72ad1877efc322e8e231163a3bba1ca862
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-06 06:46:17 -0800

    123

--- vlm_finetune/test.py
+++ vlm_finetune/test.py
@@ -68,7 +68,7 @@ def test():
     base_url = get_server_base_url()
     model_name = "Qwen/Qwen2.5-VL-7B-Instruct"
     #data_path = "/data/test.messages.jsonl"
-    data_path = "/root/finetuning-workshop/vlm_finetune/data/mini_test.jsonl"
+    data_path = "vlm_finetune/data/mini_test.jsonl"
     if not os.path.exists(data_path):
         print(f"Missing test data at {data_path}")
         return
