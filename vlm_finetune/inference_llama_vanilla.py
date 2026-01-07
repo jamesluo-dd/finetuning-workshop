e13ff54c7ab949211d92426f3e6715ee94e1347b
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-07 15:53:55 -0800

    123

--- vlm_finetune/inference_llama_vanilla.py
+++ vlm_finetune/inference_llama_vanilla.py
@@ -24,7 +24,7 @@ app = modal.App("vllm-llama-inference-vanilla")
 
 @app.function(
     image=vllm_image,
-    gpu="H100",
+    gpu="H100:3",
     #scaledown_window=240, # 4 minutes
     timeout=10 * 60,
     volumes={
