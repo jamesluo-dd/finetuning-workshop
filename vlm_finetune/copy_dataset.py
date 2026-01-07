a31204d77b2e0507a0dc1ad473ecbf53139dfb77
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-07 10:49:50 -0800

    123

--- vlm_finetune/copy_dataset.py
+++ vlm_finetune/copy_dataset.py
@@ -21,17 +21,17 @@ app = modal.App("axolotl-vlm-prep-dataset")
 def prep_dataset():
 
     src_paths = [
-        #"vlm_finetune/data/train_merchant_vs_global.jsonl",
-        #"vlm_finetune/data/val_merchant_vs_global.jsonl",
-        #"vlm_finetune/data/test_merchant_vs_global.jsonl",
+        "vlm_finetune/data/train_merchant_vs_global.jsonl",
+        "vlm_finetune/data/val_merchant_vs_global.jsonl",
+        "vlm_finetune/data/test_merchant_vs_global.jsonl",
         "vlm_finetune/data/golden_merchant_vs_global.jsonl",
     ]
 
     dst_paths = [
-        #DATA_VOLUME_DIR / "mvg_train.jsonl",
-        #DATA_VOLUME_DIR / "mvg_eval.jsonl",
-        #DATA_VOLUME_DIR / "mvg_test.jsonl",
-        DATA_VOLUME_DIR / "golden_test.jsonl",
+        DATA_VOLUME_DIR / "mvg_train.jsonl",
+        DATA_VOLUME_DIR / "mvg_eval.jsonl",
+        DATA_VOLUME_DIR / "mvg_test.jsonl",
+        DATA_VOLUME_DIR / "mvg_golden.jsonl",
     ]
 
     # def convert_one(src: Path, dst: Path) -> None:
