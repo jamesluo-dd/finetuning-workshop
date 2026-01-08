323c696d57c3badd372db6958aaab12f7ec72998
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-08 06:31:01 -0800

    123

--- vlm_finetune/test_llama_vanilla.py
+++ vlm_finetune/test_llama_vanilla.py
@@ -70,7 +70,7 @@ def test():
     base_url = get_server_base_url()
     #model_name = "neuralmagic/Llama-3.2-11B-Vision-Instruct-FP8-dynamic"
     model_name = "HuggingFaceTB/SmolVLM2-2.2B-Instruct"
-    data_path = "/data/mvg_eval.jsonl"
+    data_path = "/data/mvg_golden.jsonl"
     if not os.path.exists(data_path):
         print(f"Missing test data at {data_path}")
         return
