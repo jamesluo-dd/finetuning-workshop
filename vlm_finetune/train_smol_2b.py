09b7cfd975c22906b00e61fdffb051eb3ee1e833
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-08 08:17:40 -0800

    123

--- vlm_finetune/train_smol_2b.py
+++ vlm_finetune/train_smol_2b.py
@@ -8,7 +8,7 @@ from .common import checkpoints_volume, data_volume
 CONFIG_FILE_PATH = Path("/config_smol_2b.yml")
 
 axolotl_image = (
-    modal.Image.from_registry("axolotlai/main-20260108-py3.11-cu128-2.8.0")
+    modal.Image.from_registry("axolotlai/main-latest")
     .pip_install(
         "huggingface_hub",
         "hf-transfer",
