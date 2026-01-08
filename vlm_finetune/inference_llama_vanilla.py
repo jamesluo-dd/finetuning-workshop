25c2cd811554735d6b5d3b37515d36893194a748
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-07 18:41:10 -0800

    123

--- vlm_finetune/inference_llama_vanilla.py
+++ vlm_finetune/inference_llama_vanilla.py
@@ -54,7 +54,7 @@ def serve():
         "--no-enforce-eager",
         "--kv-cache-dtype", "fp8", 
         "--max-model-len", "8192" ,
-        "--quantization","fp8",
+        "--quantization","fp8"
         #"--load-format", "fp8" 
         # "--tensor-parallel-size", str(N_GPU)
     ]
