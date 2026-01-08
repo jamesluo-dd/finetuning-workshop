c25639cc5673fbb16fa1fc419e957eea73c16d43
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-08 01:27:11 -0800

    123

--- vlm_finetune/inference_llama_vanilla.py
+++ vlm_finetune/inference_llama_vanilla.py
@@ -65,7 +65,7 @@ def serve():
         "--kv-cache-dtype", "fp8", 
         "--max-model-len", "8192" ,
         #"--quantization","fp8",
-        "--gpu-memory-utilization", "0.60",
+        "--gpu-memory-utilization", "0.30",
         "--limit-mm-per-prompt", "image=2",
        "--quantization", "bitsandbytes", "--load-format", "bitsandbytes"
         #"--load-format", "fp8" 
