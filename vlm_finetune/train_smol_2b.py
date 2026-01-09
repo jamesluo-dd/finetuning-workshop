d738ba6a3b22d69d387941cf00bd56d6b5f303b3
jamesluo-dd <zhangqi.luo@doordash.com>
2026-01-09 00:45:50 -0800

    123

--- vlm_finetune/train_smol_2b.py
+++ vlm_finetune/train_smol_2b.py
@@ -34,8 +34,8 @@ app = modal.App("axolotl-vlm-finetune_smolvlm_2b")
 CKPT_VOLUME_DIR = Path("/checkpoints")
 DATA_VOLUME_DIR = Path("/data")
 
-LORA_OUTPUT_DIR = CKPT_VOLUME_DIR / "vlm-lora-out_smolvlm_2b_ZL"
-MERGED_OUTPUT_DIR = CKPT_VOLUME_DIR / "vlm-merged-out_smolvlm_2b_ZL"
+LORA_OUTPUT_DIR = CKPT_VOLUME_DIR / "vlm-lora-out_smolvlm_2b_gptlabel_ZL"
+MERGED_OUTPUT_DIR = CKPT_VOLUME_DIR / "vlm-merged-out_smolvlm_2b_gptlabel_ZL"
 
 
 @app.function(
